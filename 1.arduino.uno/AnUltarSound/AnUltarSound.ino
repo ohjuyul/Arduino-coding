1f4b396ae4178d511bfe05ec235bc3b24fa52fe5
ohjuyul <128760183+ohjuyul@users.noreply.github.com>
2023-04-03 16:27:12 +0900

    Create AnUltarSound.ino

---
+++ 1.arduino.uno/AnUltarSound/AnUltarSound.ino
@@ -0,0 +1,17 @@
+int trigPin = 3; 
+int echoPin = 10;  
+void setup() {
+Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+void loop() {
+  float duration, distance;
+  digitalWrite(trigPin, HIGH);   delay(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = ((float)(340 * duration) / 10000) / 2;
+  Serial.print("Duration:");     Serial.print(duration);
+  Serial.print("\nDistance:");  Serial.print(distance);
+  Serial.println("cm\n");       delay(500);
+}
