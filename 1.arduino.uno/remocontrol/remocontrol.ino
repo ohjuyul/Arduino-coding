523e24b1b5d9a336dad47442a5737bb92cb7bcdc
ohjuyul <128760183+ohjuyul@users.noreply.github.com>
2023-04-10 15:35:35 +0900

    Create remocontrol.ino

---
+++ 1.arduino.uno/remocontrol/remocontrol.ino
@@ -0,0 +1,20 @@
+#include <IRremote.h>
+int RECV_PIN = 11;
+IRrecv irrecv(RECV_PIN); decode_results results;
+void setup()
+{ pinMode(5, OUTPUT);   
+  Serial.begin(9600);
+Serial.println("Enabling IRin");
+irrecv.enableIRIn();
+Serial.println("Enabled IRin");  }
+void loop() {
+   if (irrecv.decode(&results)) {
+  Serial.println(results.value, HEX);
+  irrecv.resume(); // Receive the next value
+  if (results.value == 0xFF30CF) digitalWrite(5, HIGH);  //1번 LED 켜기
+  if (results.value == 0xFF18E7) digitalWrite(5, LOW);  // 2번 LED 끄기
+ }
+  if (irrecv.decode(&results)) {
+  Serial.println(results.value, HEX); 
+  irrecv.resume();
+} delay(100); }
